5078a5aac013825ceb9b342f5276a3ec7c6a84eb
Gerdie <mariakmoy@gmail.com>
2018-09-17 22:07:54 -0700

    Lay Tiles on Start

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+	[SerializeField] private Sprite[] images;
+	private FarmTile[] farmTiles;
+	public FarmTile baseTile;
+
+	// Use this for initialization
+	void Start () {
+		float[] xPositions = {-0.439f, 0.45f, 0.454f, 1.338f, 1.34f, 1.346f, 2.217f, 2.221f, 3.109f};
+		float[] yPositions = {-0.53f, -1.27f, 0.21f, -0.536f, -2.01f, 0.952f, 0.217f, -1.274f, -0.53f};
+
+		for ( int i=0; i < xPositions.Length; i++ ) {
+			FarmTile currentTile = Instantiate(baseTile) as FarmTile;
+			int randomImgIndex = Random.Range (0, images.Length);
+			currentTile.LayTile (i, xPositions[i], yPositions[i], images[randomImgIndex]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/FarmTile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmTile : MonoBehaviour {
+	private int _id;
+
+	public int id {
+		get { return _id; }
+	}
+
+	// Used for initialization
+	public void LayTile (int _id, float xPos, float yPos, Sprite image) {
+		GetComponent<SpriteRenderer> ().sprite = image;
+		this.transform.position = new Vector3 (xPos, yPos, -0.1f);
+	}
+
+}
