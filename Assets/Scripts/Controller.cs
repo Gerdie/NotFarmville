b103e489dfc5cd698c82ccf953892a564714d0d1
Gerdie <mariakmoy@gmail.com>
2018-09-18 22:03:29 -0700

    Plow fields OnMouseDown

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -11,11 +11,13 @@ public class Controller : MonoBehaviour {
 	void Start () {
 		float[] xPositions = {-0.439f, 0.45f, 0.454f, 1.338f, 1.34f, 1.346f, 2.217f, 2.221f, 3.109f};
 		float[] yPositions = {-0.53f, -1.27f, 0.21f, -0.536f, -2.01f, 0.952f, 0.217f, -1.274f, -0.53f};
+		farmTiles = new FarmTile[xPositions.Length * yPositions.Length];
 
 		for ( int i=0; i < xPositions.Length; i++ ) {
 			FarmTile currentTile = Instantiate(baseTile) as FarmTile;
 			int randomImgIndex = Random.Range (0, images.Length);
 			currentTile.LayTile (i, xPositions[i], yPositions[i], images[randomImgIndex]);
+			farmTiles [i] = currentTile;
 		}
 	}
 	
--- Assets/Scripts/FarmTile.cs
+++ Assets/Scripts/FarmTile.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FarmTile : MonoBehaviour {
+	public Sprite plowedImg;
 	private int _id;
+	private bool plowed;
 
 	public int id {
 		get { return _id; }
@@ -13,6 +15,16 @@ public class FarmTile : MonoBehaviour {
 	public void LayTile (int _id, float xPos, float yPos, Sprite image) {
 		GetComponent<SpriteRenderer> ().sprite = image;
 		this.transform.position = new Vector3 (xPos, yPos, -0.1f);
+		plowed = false;
+	}
+
+	public void Plow() {
+		GetComponent<SpriteRenderer> ().sprite = plowedImg;
+		plowed = true;
+	}
+
+	public void OnMouseDown() {
+		Plow ();
 	}
 
 }
